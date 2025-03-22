e57a85f7c40104b536d10324d04f4a24e738561e
ShubhamChoure <shubhamchoureps@gmail.com>
2025-03-22 22:52:15 +0530

    changed es lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,13 +9,25 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Get the base ESLint config from Next.js
 const rawConfig = [...compat.extends("next/core-web-vitals")];
 
-// Remove the parser.parse function if it exists
+// Process each config object to ensure no unsupported function values are present
 const eslintConfig = rawConfig.map((config) => {
-  if (config.parser && typeof config.parser.parse === "function") {
-    const { parse, ...rest } = config.parser;
-    config.parser = rest;
+  if (config.parser && typeof config.parser === "object") {
+    // Remove any function values from the parser object (specifically the "parse" function)
+    if (typeof config.parser.parse === "function") {
+      const { parse, ...rest } = config.parser;
+      config.parser = rest;
+    }
+    // Set a supported parser if one is not already specified as a string
+    // Here we set it explicitly to @babel/eslint-parser; change it if needed.
+    if (typeof config.parser !== "string") {
+      config.parser = "@babel/eslint-parser";
+    }
+  } else if (!config.parser) {
+    // If no parser is defined, set one explicitly
+    config.parser = "@babel/eslint-parser";
   }
   return config;
 });
