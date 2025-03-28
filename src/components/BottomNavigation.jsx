39bda7b67b8bd7d6696597183fc5b01bfe453e86
ShubhamChoure <shubhamchoureps@gmail.com>
2025-03-29 00:23:28 +0530

    revert 1

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -19,22 +19,21 @@ export default function BottomNavigation() {
   const pathname = usePathname()
 
   return (
-    <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-gray-800 relative z-30">
+    <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-gray-800 overflow-visible relative z-30">
       <div className="flex justify-around h-16">
         {navItems.map((item) => {
           const isActive = pathname === item.href
 
           return (
-            <Link key={item.name} href={item.href} className="inline-flex">
+            <Link key={item.name} href={item.href} className="">
               <motion.div
-                className="inline-flex"
                 initial={false}
                 animate={
                   isActive
                     ? {
                         y: -10,
                         scale: 1.1,
-                        boxShadow: "0px 0px 10px rgba(168, 85, 247, 0.6)",
+                        boxShadow: "0px 0px 10px rgba(168, 85, 247, 0.6)", // Adjusted shadow color
                       }
                     : { y: 0, scale: 1, boxShadow: "none" }
                 }
@@ -43,12 +42,20 @@ export default function BottomNavigation() {
                 <Button
                   variant={isActive ? "bottomnav" : "ghost"}
                   size="icon"
-                  className={`flex flex-col items-center justify-center gap-0.5 transition-all ${
+                  className={`flex flex-col h-full w-16 items-center justify-center gap-0.5 transition-all ${
                     isActive ? "bg-purple-600" : "hover:bg-purple-600/30"
                   }`}
                 >
-                  <item.icon className="h-10 w-10 text-white" />
-                  <span className="text-[10px] font-bold leading-tight text-white">
+                  <item.icon
+                    className={`h-10 w-10 ${
+                      isActive ? "text-white" : "text-white"
+                    }`}
+                  />
+                  <span
+                    className={`text-[10px] font-bold leading-tight ${
+                      isActive ? "text-white" : "text-white"
+                    }`}
+                  >
                     {item.name}
                   </span>
                 </Button>
